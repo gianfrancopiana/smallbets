c6650178cf679c6664f7a97b5eee2d07ac84fa68
rajesh6600 <go.4rajesh7@gmail.com>
2025-10-15 23:16:31 +0530

    Added real-time broadcasting for mentions and threads (#74)
    #72 Implemented real-time channel support for threaded conversations and user mentions to enable live updates.

--- app/models/message/broadcasts.rb
+++ app/models/message/broadcasts.rb
@@ -4,6 +4,8 @@ module Message::Broadcasts
     ActionCable.server.broadcast("unread_rooms", { roomId: room.id, roomSize: room.messages_count, roomUpdatedAt: created_at.iso8601 })
 
     broadcast_notifications
+    broadcast_to_inbox_mentions
+    broadcast_to_inbox_threads
   end
 
   def broadcast_update
@@ -36,5 +38,63 @@ module Message::Broadcasts
                         partial: "messages/message",
                         locals: { message: self, current_room: room },
                         attributes: { maintain_scroll: true }
+  end
+
+  def broadcast_to_inbox_mentions
+
+    return if mentionee_ids.blank?
+    
+    mentionees.each do |user|
+      next if user.id == creator_id
+      
+      broadcast_remove_to user, :inbox_mentions,
+                         target: ActionView::RecordIdentifier.dom_id(self)
+      
+      broadcast_append_to user, :inbox_mentions,
+                          target: "inbox",
+                          partial: "messages/message",
+                          locals: { 
+                            message: self,
+                            current_room: nil,
+                            first_unread_message: nil,
+                            timestamp_style: :long_datetime,
+                            show_date_separator: true
+                          }
+    end
+  end
+
+  def broadcast_to_inbox_threads
+
+    return unless room.thread? && room.parent_message
+    
+    parent_message = room.parent_message
+    thread = room
+    
+    thread.reload
+    
+    thread_user_ids = thread.memberships.active.visible.pluck(:user_id)
+    parent_room_user_ids = parent_message.room.memberships.active.involved_in_everything.pluck(:user_id)
+    all_user_ids = (thread_user_ids + parent_room_user_ids).uniq
+    
+    all_user_ids.each do |user_id|
+      next if user_id == creator_id
+      user = User.find(user_id)
+      
+      if thread.messages_count == 1
+        broadcast_append_to user, :inbox_threads,
+                           target: "inbox",
+                           partial: "messages/message",
+                           locals: {
+                             message: parent_message,
+                             timestamp_style: :long_datetime,
+                             show_date_separator: true
+                           }
+      else
+        broadcast_replace_to user, :inbox_threads,
+                            target: ActionView::RecordIdentifier.dom_id(parent_message, :threads),
+                            partial: "messages/threads",
+                            locals: { message: parent_message.reload }
+      end
+    end
   end
 end
